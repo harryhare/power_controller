9deca7ae60afd8305a03cf3c818a48c9884309c9
harryhare <harryhare@163.com>
2018-01-02 10:52:40 +0800

    add basic auth for pdu

--- controller.py
+++ controller.py
@@ -4,7 +4,10 @@ import re
 import platform
 
 machines=["http://192.168.1.99:8081","http://192.168.1.98:8081","http://192.168.1.74:8081"]
-pdu="http://user:user@192.168.1.100/port_status.shtml"
+pdu="http://192.168.1.100/port_status.shtml"
+pdu_domain='Admin'
+pdu_uri='http://192.168.1.100'
+
 core_num=6
 if(platform.platform()=='Windows-7-6.1.7601-SP1'):
 	machines=["http://127.0.0.1:8081"]
@@ -38,6 +41,11 @@ def get_freq_all():
 	return a
 
 def get_total_power():
+	auth_handler = urllib.request.HTTPBasicAuthHandler()
+	auth_handler.add_password(realm="Admin", uri='http://192.168.1.100', user='user', passwd='user')
+	opener = urllib.request.build_opener(auth_handler)
+	# ...and install it globally so it can be used with urlopen.
+	urllib.request.install_opener(opener)
 	res=urllib.request.urlopen(pdu)
 	data=res.read().decode('utf-8')
 	m=re.search('CA:\s+(\d+.\d*)A',data)
