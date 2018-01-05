64b7ce726647ad9161b45b95364a3ddf31d3fed8
harryhare <harryhare@163.com>
2018-01-05 21:54:57 +0800

    add time_test

---
+++ time_test.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<time.h>
+int main()
+{
+int i=0;
+int c=0;
+time_t t0=time(0);
+time_t t;
+while(1){
+    i++;
+    if(i%1000000000==0){
+        c++;
+        i=0;
+        t=time(0);
+        //printf("%ld\n",(long)(t-t0));
+        t0=t;
+        break;
+    }
+}
+return 0;
+}
